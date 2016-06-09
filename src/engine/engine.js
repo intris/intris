99bd132d8624befb42991b7f666b43e2bc74daf2
IntPtr <i@intptr.im>
2016-06-09 21:35:45 +0800

    split class Engine to another file

---
+++ src/engine/engine.js
@@ -0,0 +1,43 @@
+import R from "ramda";
+
+import EngineData from "../data/engine.json";
+import GroundData from "../data/ground.json";
+import BlockData from "../data/block.json";
+
+import Block from "../structs/block";
+import Ground, {checkAvailable} from "../structs/ground";
+
+import Random from "../utils/random";
+
+export default class Engine {
+  constructor() {
+    this.random = new Random();
+    this.ground = Ground();
+    this.block = null;
+    this.checkAvailable = checkAvailable(this.ground);
+    this.delays = {
+      drop: 0
+    };
+    this.nexts = R.times(::this.randomType, EngineData.next);
+  }
+  randomType() {
+    return this.random.next(BlockData.types.length);
+  }
+  nextBlock() {
+    this.block = Block({
+      type: R.head(this.nexts),
+      x: Math.floor((GroundData.size.width - BlockData.size.width) / 2 + BlockData.offset.x),
+      y: Math.floor(-BlockData.size.height / 2 + BlockData.offset.y)
+    });
+    this.nexts = R.append(this.randomType(), R.tail(this.nexts));
+  }
+  next({count, config, input}) {
+    return {
+      action: "next",
+      data: {
+        ground: this.ground,
+        block: this.block
+      }
+    };
+  }
+}
--- src/engine/index.js
+++ src/engine/index.js
@@ -1,47 +1,7 @@
 import R from "ramda";
 import create from "@most/create";
 
-import EngineData from "../data/engine.json";
-import GroundData from "../data/ground.json";
-import BlockData from "../data/block.json";
-
-import Block from "../structs/block";
-import Ground, {checkAvailable} from "../structs/ground";
-
-import Random from "../utils/random";
-
-class Engine {
-  constructor() {
-    this.random = new Random();
-    this.ground = Ground();
-    this.block = null;
-    this.checkAvailable = checkAvailable(this.ground);
-    this.delays = {
-      drop: 0
-    };
-    this.nexts = R.times(::this.randomType, EngineData.next);
-  }
-  randomType() {
-    return this.random.next(BlockData.types.length);
-  }
-  nextBlock() {
-    this.block = Block({
-      type: R.head(this.nexts),
-      x: Math.floor((GroundData.size.width - BlockData.size.width) / 2 + BlockData.offset.x),
-      y: Math.floor(-BlockData.size.height / 2 + BlockData.offset.y)
-    });
-    this.nexts = R.append(this.randomType(), R.tail(this.nexts));
-  }
-  next({count, config, input}) {
-    return {
-      action: "next",
-      data: {
-        ground: this.ground,
-        block: this.block
-      }
-    };
-  }
-}
+import Engine from "./engine";
 
 export default R.curry(({config, input}, ticker) =>
   create((next, complete, error) => {
